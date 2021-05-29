4856d98e0c4ef0349e06cc3d3e1146a762bc27b7
SABIQ <ammarraneez@gmail.com>
2021-05-29 19:48:54 +0530

    small adjustments to redux

--- src/redux/reducers/currentlySwappingReducer.js
+++ src/redux/reducers/currentlySwappingReducer.js
@@ -5,9 +5,9 @@ const initialState = [];
 export const currentSwappers = handleActions({
     SET_CURRENT_SWAPPERS: (state, { payload }) => {
         if (payload.length) {
-        return state.concat(payload);
+            return state.concat(payload);
         } else {
-        return [];
+            return [];
         }
     },
 }, initialState);
